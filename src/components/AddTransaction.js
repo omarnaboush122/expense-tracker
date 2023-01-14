04f592fec0fb4f1f2b9fe1d6dceefe7dcb8cd15b
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-14 14:25:25 -0800

    add inputs state

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,35 @@
+import { useState } from "react";
 
 
 const AddTransaction = () => {
+  const [inputs,setInputs] = useState({
+    text: "",
+    amount: ""
+  });
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    setInputs(prevInputs => {
+      return {
+        ...prevInputs,
+        [name]: value
+      }
+    })
+  } 
+  
   return (
     <>
     <h3>Add new transaction</h3>
       <form >
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text"   placeholder="Enter text..." />
+          <input type="text" value={inputs.text} name="text" onChange={handleChange} placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
             >Amount <br />
             (negative - expense, positive - income)</label>
-          <input type="number"  placeholder="Enter amount..." />
+          <input type="number" value={inputs.amount} name="amount" onChange={handleChange} placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
