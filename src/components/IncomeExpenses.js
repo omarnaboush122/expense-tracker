35527afc4ec58d5f0573a56ff0ebfa4ab14c441e
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-14 16:30:40 -0800

    fix total expense

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,33 +1,31 @@
 import { useContext } from "react";
 import { Context } from "./Context";
 
-
-
 const IncomeExpenses = () => {
   const transactions = useContext(Context);
-  const amount = transactions.map(transaction => transaction.amount)
-  const income = amount.filter(amount => amount > 0);
-  const totalIncome = income.reduce((acc,curr) => {
-    return acc + curr;
-  },0).toFixed(2);
-  
-  const expense = amount.filter(amount => amount < 0);
-  const totalExpense = expense.reduce((acc,curr) => {
-    return acc + curr;
-  },0).toFixed(2);
+  const amount = transactions.map((transaction) => transaction.amount);
+  const income = amount.filter((amount) => amount > 0);
+  const totalIncome = income
+    .reduce((acc, curr) => {
+      return acc + curr;
+    }, 0)
+    .toFixed(2);
 
+  const expense = amount.filter((amount) => amount < 0);
+  const totalExpense = expense.reduce((acc, curr) => acc + curr, 0) * -1;
+  const totalExpenseFixed = totalExpense.toFixed(2);
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+${totalIncome}</p>
+        <p className="money plus">{totalIncome}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-${totalExpense}</p>
+        <p className="money minus">{totalExpenseFixed}</p>
       </div>
     </div>
   );
-}
+};
 
 export default IncomeExpenses;
